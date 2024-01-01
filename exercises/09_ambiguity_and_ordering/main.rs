e5a3bf4a1a98b47e02139b8fa20ca01949759754
sergey.gerodes <sgerodes@gmail.com>
2024-01-01 19:54:38 +0100

    solves 09

--- exercises/09_ambiguity_and_ordering/main.rs
+++ exercises/09_ambiguity_and_ordering/main.rs
@@ -24,23 +24,23 @@ impl NumberType {
 
 // Sum together at least two expressions.
 macro_rules! sum {
-    ($($expr:expr),+ , $lastexpr:expr) => {
-        $($expr + )+ $lastexpr
+    ($firstexpr:expr $(,$expr:expr)+) => {
+        $firstexpr $( + $expr)+
     }
 }
 
 macro_rules! get_number_type {
-    ( $e:expr ) => {
-        NumberType::UnknownBecauseExpr($e)
-    };
-    ( $block:block ) => {
-        NumberType::UnknownBecauseBlock($block)
+    ( -$negative:literal ) => {
+        NumberType::NegativeNumber(-$negative)
     };
     ( $positive:literal ) => {
         NumberType::PositiveNumber($positive)
     };
-    ( -$negative:literal ) => {
-        NumberType::NegativeNumber(-$negative)
+    ( $block:block ) => {
+        NumberType::UnknownBecauseBlock($block)
+    };
+    ( $e:expr ) => {
+        NumberType::UnknownBecauseExpr($e)
     };
 }
 
